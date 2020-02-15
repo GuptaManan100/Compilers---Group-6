1864a930d83e07ba4ea27df16083c6da3e817056
ThePiyushGupta <piyushg1411@gmail.com>
2020-02-15 12:36:26 +0530

    idk3

--- Assignment 2/testing.py
+++ Assignment 2/testing.py
@@ -15,16 +15,45 @@ def removeComments(string):
 
 
 content = removeComments(content)
-print(content)
+# print(content)
 testre = re.compile("class\s+([a-zA-Z][a-zA-Z0-9]*)\s*\{", re.DOTALL)
 testclass = re.findall(testre, content)
-print(testclass)
+# print(testclass)
 
 testre = re.compile(
     "class\s+([a-zA-Z][a-zA-Z0-9]*)\s+extends\s+([a-zA-Z][a-zA-Z0-9]*)\s*\{", re.DOTALL)
 testinclass = re.findall(testre, content)
 inclass = [x[0] for x in testinclass]
-print(inclass)
+# print(inclass)
 for x in inclass:
     testclass.append(x)
-print(testclass, testinclass)
+print("Classes:", testclass, "Inherited classes:", testinclass)
+
+
+sum1 = 0
+for name in testclass:
+    regex = "public\s+" + name
+    testre = re.compile(regex, re.DOTALL)
+    cons = re.findall(testre, content)
+    # print( cons)
+    sum1 += len(cons)
+
+    regex = "private\s+" + name
+    testre = re.compile(regex, re.DOTALL)
+    cons = re.findall(testre, content)
+    sum1 += len(cons)
+
+    regex = "protected\s+" + name
+    testre = re.compile(regex, re.DOTALL)
+    cons = re.findall(testre, content)
+    sum1 += len(cons)
+
+print("Constructors:", sum1)
+
+numObjectDeclarations = 0
+for item in testclass:
+    item += '\s+[a-zA-z][a-zA-Z0-9]*\s*='
+    x = re.findall(item, content)
+    for items in x:
+        numObjectDeclarations += 1
+print("Objects:", numObjectDeclarations)
