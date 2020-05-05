eb190bddb0d9a1aab0ae188dc4b5b42863790dfb
Anubhav Tyagi <tyagianubhav619@gmail.com>
2020-05-05 10:54:56 +0530

    test1.c and test2.c are giving segmentation fault

---
+++ Assignment 5/Tests/test1.c
@@ -0,0 +1,23 @@
+int add(int a,int b){
+    return a+b;
+}
+
+int main(){
+    int x;
+    x=1;
+    x =x * 2;
+    int y;
+    y=x;
+    y = y+x;
+    y = y/4;
+    if(y==2){
+        x = 2;
+    }
+    else{
+
+    }
+    float z;
+    z = x;
+    x = add(2,x*y);
+    
+}
---
+++ Assignment 5/Tests/test2.c
@@ -0,0 +1,35 @@
+int add(int a,int b){
+    return a+b;
+}
+
+int main(){
+    int x;
+    x=1;
+    x =x * 2;
+    int y;
+    y=x;
+    y = y+x;
+    y = y/4;
+    
+    switch(x+2*y){
+        case 12: int x = 3;
+                 y = 4;
+        default:
+    }
+
+    while(x<3){
+        int z;
+        z = x;
+        x=x+1;
+        while(y>2){
+            y=y-1;
+            switch(y){
+                case 1: x = 1;
+            }
+        }
+    }
+    
+    
+}
+
+
